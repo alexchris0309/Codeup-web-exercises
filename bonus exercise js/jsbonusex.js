769e70856e7f7ec8046d1da774a1fc2d5c8eaecb
Alexandra <Alexandra@Julias-MacBook-Pro.local>
2019-06-24 16:03:53 -0500

    gitignore update

--- bonus exercise js/jsbonusex.js
+++ bonus exercise js/jsbonusex.js
@@ -5,10 +5,10 @@ var fruits = ["apple", "banana", "orange", "apple", "pineapple"]
 
 
 
-function allIndexesOf(array,element){
-    for(var i =0; i < array.length; i ++)
-        console.log( element +" has the index of " +  );
-
+function allIndexesOf(array,element)
+    {
+        var index = array.indexOf( element);
+        console.log(index);
 
 }
 
