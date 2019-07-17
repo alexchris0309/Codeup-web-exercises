83863ea5ddbcb99e3fac85b0c18f33b9c01e39cd
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-17 18:49:31 -0500

    update to loops

--- js/for_loop.js
+++ js/for_loop.js
@@ -1,47 +1,27 @@
-//
-// // function multiplication(num){
-// //     return i * num;
-// // }
-// //
-// // for (var i = 0; i <=10;i++){
-// //     console.log( "7 x " + i  + " = " + multiplication(7));
-//
-// var showMultiplicationTable= function(num){
-//     for (var i =1; i <=10;i++);
-//     console.log(num + " x " + i + " = " +(i * num));
-// }
-// }
-// showMultiplicationTable(7)
-//
-//
-//
-// // is odd/even
-//
-// for ( var i = 0; i <=10; i ++){
-//     var ran= Math.floor(Math.random() * 180) + 20;
-//     if (ran % 2 === 0){
-//         console.log(ran + " is even!")
-// }else{
-//         console.log( ran + " is odd!");
-//     }
-// }
-//
-//
-//
-//
-//
-// // function createHalfPyramid(height) {
-// //
-// //     for (var i = 1; i <= height; i++) {
-// //         var row = "";
-// //
-// //         for (var j = 1; j <= i; j++) {
-// //             row += "" + i;
-// //         }
-// //
-// //         console.log(row);
-// //     }
-// // }
+
+
+ showMultiplicationTable= function(num){
+    for (var i =1; i <=10;i++){
+    console.log(num + " x " + i + " = " +(i * num));
+}
+
+showMultiplicationTable(7)
+
+
+
+// is odd/even
+
+for ( var i = 0; i <=10; i ++){
+    var ran = Math.floor(Math.random() * 180) + 20;
+    if (ran % 2 === 0){
+        console.log(ran + " is even!")
+}else{
+        console.log( ran + " is odd!");
+    }
+}
+
+
+
 
 var christmasTree= function(){
     for (var outer = 1; outer <= 9; outer ++){
@@ -51,18 +31,10 @@ var christmasTree= function(){
         }
     console.log(output)
     }
-}
-// //
-// // var christmasTree = function(){
-// //     for (var outer = 1; outer <=9;outer ++){
-// //         console.log(String(outer).repeat(outer));
-// //     }
-// // }
-//
-// //
-// // christmasTree();
-//
-//
-// for (i = 100; i >= 5; i = i - 5){
-//     console.log(i);}
+
+console.log(christmasTree())
+
+
+for (i = 100; i >= 5; i = i - 5){
+    console.log(i);}
 
--- js/while.js
+++ js/while.js
@@ -1,33 +1,52 @@
 "use strict;"
 
 
+
 //
-// //
-// var i = 1;
+var i = 1;
+
+while (i< 65536){
+i = i * 2
+
+    console.log( i)
+}
+
+
+
 //
-// while (i< 65536){
-// i = i * 2
+// var allCones = (Math.floor(Math.random() * 50) + 50);
+// console.log("starting the day with "+ allCones + " cones");
+// do {
 //
-//     console.log( i)
-// }
+//     var conesWanted = Math.floor((Math.random() * 5) + 1);
+//     if (conesWanted <= allCones) {
+//         allCones -= conesWanted;
+//         console.log(conesWanted + "cones sold..");
+//     } else {
+//         console.log("cann ot see you  " + conesWanted + " cones I only have " + allcones + "....");
+//
+//     }
+// } while (allCones>0);
+// console.log("yay, I sold them all!");
 
 
 
 
-var allCones = (Math.floor(Math.random() * 50) + 50);
-console.log("starting the day with "+ allCones + " cones");
-do {
+ var allCones = Math.floor(Math.random() * 50) + 50;
 
-    var conesWanted = Math.floor((Math.random() * 5) + 1);
-    if (conesWanted <= allCones) {
-        allCones -= conesWanted;
-        console.log(conesWanted + "cones sold..");
-    } else {
-        console.log("cann ot see you  " + conesWanted + " cones I only have " + allcones + "....");
-
-    }
-} while (allCones>0);
-console.log("yay, I sold them all!");
+ console.log( "I need to sell " + allCones + " today.")
 
 
+ do{
+     var cusBuy = Math.floor(Math.random() * 5) + 1;
+     if (allCones >= cusBuy){
+         allCones = allCones -cusBuy;
+         console.log("Yes I can sell you " + cusBuy + " I have " + allCones + " cones left");
+     }
+     else {
+         console.log(" No I can not sell you " + cusBuy + " I have " + allCones + " cones left");
+     }
+ }
 
+ while ( allCones > 0);
+ console.log( "Yay I sold all the cones!")
