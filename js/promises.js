4e6c0d6b30eeb4198ad673eda8a852e31449e4a9
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-18 19:29:55 -0500

    promise exercise

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,44 @@
 // Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
 
 
-function wait(number) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            {
-                resolve('this has been resolved');
-            } else {
-                reject('Network Connection Error!');
-            }
-        }, 2000);
+const wait=(number) =>{
+    return new Promise((resolve) => {
+        setTimeout(resolve, number);
     });
 }
 
 
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//
+//
+//
+//
 
 
+
+
+function commits(username) {
+    return fetch(`https://api.github.com/users/${username}/events/public`, {headers: {'Authorization':`token ${githubkey}`}})
+        .then (response=> response.json())
+        .then ((data) => {
+            // console.log(data)
+
+
+            const userData = data.filter((gitStat) => {
+                // console.log(gitStat);
+                if (gitStat.type === "PushEvent") {
+                    return gitStat
+                }
+            });
+
+            console.log(userData[0].created_at)
+        })
+        .catch(error => console.error(error))
+
+}
+
+// commits("alexchris0309");
+
+let username = prompt( "what user do you want to check?");
+commits(username)
