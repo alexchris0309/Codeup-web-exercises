4ae2685ee11cd230f76f878672a59af2c1b92182
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-18 19:32:08 -0500

    change to fizzbuzz and add promise HTML

--- bonus exercise js/fizzbuzz.js
+++ bonus exercise js/fizzbuzz.js
@@ -3,28 +3,25 @@
 
 
 
+const fizzbuzz=(input)=>{
+    for (let i = 1; i <= input; i++) {
+        if (i % 15 === 0) {
+            console.log("fizzbuzz ");
+        } else if (i % 3 === 0) {
+            console.log("fizz");
+        } else if (i % 5 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    }
 
- for(var i = 1; i <= 50; i++) {
-     if (i % 5 === 0) {
-         console.log("buzz ");
-     } else if (i % 3 === 0) {
-         console.log("fizz");
-     } else if (i % 15 === 0)
-     {
-         console.log("fizzbuzz");
-     }
- else
-     {
-         console.log(i)
-     }
- }
-
+}
 
 
 
 
-
-
+console.log(fizzbuzz(50))
 
 
 
